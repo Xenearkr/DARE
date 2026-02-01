219e042b22039df6e131a2e5af2437ba7b3efa56
yjyddq <1004328545@qq.com>
2026-02-02 04:06:21 +0800

    update opencompass/sdar_examples

---
+++ opencompass/sdar_examples/lmdeploy_sdar_1dot7b_chat_gen_mbpp_length4096.py
@@ -0,0 +1,58 @@
+from mmengine.config import read_base
+with read_base():
+    from ..opencompass.configs.datasets.mbpp.sanitized_mbpp_gen_742f0c import \
+        sanitized_mbpp_datasets
+    from ..opencompass.configs.models.dllm.lmdeploy_sdar_1dot7b_chat import \
+        models as lmdeploy_sdar_1dot7b_chat
+datasets = sanitized_mbpp_datasets
+models = lmdeploy_sdar_1dot7b_chat
+confidence_threshold = 0.95
+if 0 < confidence_threshold < 1.0:
+    dllm_unmasking_strategy = "low_confidence_dynamic"
+elif confidence_threshold == 1.0:
+    dllm_unmasking_strategy = "low_confidence_static"
+eval_cfg = {
+    'engine_config': {
+        'session_len': 8192, 
+        'max_batch_size': 16, 
+        'tp': 1,
+        'dtype': "float16",
+        'max_prefill_token_num': 4096,
+        'cache_max_entry_count': 0.8,
+        'dllm_block_length': 4,
+        'dllm_denoising_steps': 4,
+        'dllm_unmasking_strategy': dllm_unmasking_strategy,
+        'dllm_confidence_threshold': confidence_threshold,
+    },
+    'gen_config': {
+        'top_k': 50, 
+        'temperature': 1.0, 
+        'top_p': 0.95, 
+        'do_sample': False, 
+        'max_new_tokens': 4096,
+    },
+    'max_seq_len': 8192,
+    'max_out_len': 4096,
+    'batch_size': 16,
+}
+
+
+for model in models:
+    model.update(eval_cfg)
+from opencompass.partitioners import NumWorkerPartitioner
+from opencompass.runners import LocalRunner
+from opencompass.tasks import OpenICLInferTask
+infer = dict(
+    partitioner=dict(
+        type=NumWorkerPartitioner,
+        num_worker=8,   
+        num_split=None,   
+        min_task_size=16, 
+    ),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=64,
+        task=dict(type=OpenICLInferTask),
+        retry=5
+    ),
+)
---
+++ opencompass/sdar_examples/sdar_1dot7b_chat_gen_math_length4096.py
@@ -0,0 +1,50 @@
+from mmengine.config import read_base
+with read_base():
+    from ..opencompass.configs.datasets.math.math_prm800k_500_0shot_cot_gen_11c4b5 import \
+        math_datasets
+    from ..opencompass.configs.models.dllm.sdar_1dot7b_chat import \
+        models as sdar_1dot7b_chat
+datasets = math_datasets
+models = sdar_1dot7b_chat
+summarizer = dict(
+    summary_groups=sum([v for k, v in locals().items() if k.endswith('_summary_groups')], []),
+)
+eval_cfg = {
+    'gen_length': 4096,
+    'block_length': 4,
+    'denoising_steps': 4, 
+    'batch_size': 1, 
+    'batch_size_': 1,
+    'model_kwargs': {
+        'attn_implementation': 'flash_attention_2',  #'sdpa'
+        'torch_dtype': 'bfloat16',
+        'device_map': 'auto',
+        'trust_remote_code': True,
+    },
+    'temperature': 1.0,
+    'top_k': 1, 
+    'top_p': 1.0,
+    'confidence_threshold': 1.0,
+    'remasking': 'low_confidence_static',
+}
+
+for model in models:
+    model.update(eval_cfg)
+from opencompass.partitioners import NumWorkerPartitioner
+from opencompass.runners import LocalRunner
+from opencompass.tasks import OpenICLInferTask
+infer = dict(
+    partitioner=dict(
+        type=NumWorkerPartitioner,
+        num_worker=8,    
+        num_split=None,   
+        min_task_size=16, 
+    ),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=64,
+        task=dict(type=OpenICLInferTask),
+        retry=5
+    ),
+)
+
